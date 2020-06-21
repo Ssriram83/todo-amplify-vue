fcd1e70ca4982a20d106b68bd3c3d10ebf201d6a
sriram <sriram+techworkshops@users.noreply.github.com>
2020-06-21 17:48:14 +0530

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Amplify from 'aws-amplify';
+import '@aws-amplify/ui-vue';
+import aws_exports from './aws-exports';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import "bootswatch/dist/sketchy/bootstrap.min.css"; 
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Amplify.configure(aws_exports);
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
